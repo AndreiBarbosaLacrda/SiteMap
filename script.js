ca6631b763892fc3912e265149cdc02622a0c7a7
Vitoria <vitoria.limaa0112@gmail.com>
2024-12-01 11:22:16 -0300

    tirei o slideshow

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let slideIndex = 1;
 showSlides(slideIndex);
- 
+
 // Next/previous controls
 function plusSlides(n) {
     showSlides(slideIndex += n);
@@ -9,6 +9,7 @@ function plusSlides(n) {
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
+
 function showSlides(n) {
     let i;
     if (document.querySelector(".carousel")) {
@@ -16,20 +17,15 @@ function showSlides(n) {
         if (slideshowPage.classList.contains("carousel")) {
             let slides = slideshowPage.querySelectorAll(".mySlides");
             if (n > slides.length) {
-            slideIndex = 1
-            };
+                slideIndex = 1;
+            }
             if (n < 1) {
-            slideIndex = slides.length
-            };
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        };
-        slides[slideIndex-1].style.display = "block";
+                slideIndex = slides.length;
+            }
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            slides[slideIndex-1].style.display = "block";
         }
     }
 }
- 
- 
-setInterval(function() {
-  plusSlides(1)
-}, 6000);
